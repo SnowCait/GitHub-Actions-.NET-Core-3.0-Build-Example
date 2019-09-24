b569eaff5e6bb33cb476356cc2302ac7d78e817a
SnowCait <SnowCait@users.noreply.github.com>
2019-09-24 20:28:14 +0900

    Create project

---
+++ GitHub-Actions-.NET-Core-3.0-Build-Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_Actions_.NET_Core_3._0_Build_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
